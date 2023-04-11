6825997337acde1f0c91c8d0b042c07495bb3b01
Amrit Pandey <mail.amritpandey@gmail.com>
2023-04-11 19:01:38 +0530

    session 1: flask, jinja

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# msg = "Hello, Amrit"
+website_name = "Readaholic"
+data = {
+    "website_name": "Readaholic",
+    "author": "Amrit"
+}
+
+# root route (directs to root directory of project)
+@app.route("/")
+def home():
+    # rendering(returning) HTML file
+    return render_template("home.html", data=data)
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+# grouping of routes together
+# @app.route("/route1")
+# @app.route("/route2")
+# @app.route("/route3")
+# def route1():
+#     return f"<h1>{msg}</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
